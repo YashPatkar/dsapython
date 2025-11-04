81a0e2caf509398216d5a02fd9ba18f6ed920264
YashPatkar <yash.patkar2004@gmail.com>
2025-11-04 18:26:42 +0530

    Complete: Check if an array is sorted

--- test.py
+++ test.py
@@ -1,17 +1,8 @@
-def sortit(nums):
-    if len(nums) <= 1:
-        return nums
-    pivot = nums[0]
-    left, right = [], []
-    for i in nums:
-        if i <= pivot:
-            left.append(i)
-        else:
-            right.append(i)
-    return sortit(left) + [pivot] + sortit(right)
-
-
-def getSecondOrderElements(n: int,  a: [int]) -> [int]:
-    nums = sortit(a)
-    result = [nums[n - 2], nums[1]]
-    return result
+# Question: Check if an array is sorted
+class Solution:
+    def isSorted(self, arr) -> bool:
+        nums = arr
+        for i in range(1, len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                return False
+        return True
