28ca9387968b32090aea47490b3e39970a1d3d49
YashPatkar <yash.patkar2004@gmail.com>
2025-11-04 18:12:44 +0530

    Complete - Find second largest element from an array

--- test.py
+++ test.py
@@ -1,24 +1,17 @@
-class Solution:
-    def func(self, nums):
-      if len(nums) <= 1:
+def sortit(nums):
+    if len(nums) <= 1:
         return nums
-        
-      largest = nums[0]
-      for i in nums[1:]:
-        if i >= largest:
-          largest = i
-      
-      second_largest = -1
-      for i in nums:
-        if i >= second_largest and i != largest:
-          second_largest = i
+    pivot = nums[0]
+    left, right = [], []
+    for i in nums:
+        if i <= pivot:
+            left.append(i)
+        else:
+            right.append(i)
+    return sortit(left) + [pivot] + sortit(right)
 
-      return second_largest
-      
-    def getSecondLargest(self, arr):
-        return self.func(arr)
-    
-solution = Solution()
-nums = [10, 10, 10]
-call = solution.getSecondLargest(nums)
-print(call)
+
+def getSecondOrderElements(n: int,  a: [int]) -> [int]:
+    nums = sortit(a)
+    result = [nums[n - 2], nums[1]]
+    return result
