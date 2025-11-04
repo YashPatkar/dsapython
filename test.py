dba40922b9d9d00a5db6eb6c676b9d37ccdbb587
YashPatkar <yash.patkar2004@gmail.com>
2025-11-04 13:45:33 +0530

    Completed (used sorting): Find second largest element from an array

--- test.py
+++ test.py
@@ -1,32 +1,20 @@
 class Solution:
-    def largestFunc(self, arr):
-      largest = arr[0]
-      index = 0
-      for i in range(len(arr)):
-        if arr[i] >= largest:
-          largest = arr[i]
-          index = i
-      return [index, largest]
-      
+     
+    def func(self, nums):
+        nums = sorted(nums)
+        largest = nums[len(nums) - 1]
+        second_largest = float('-inf')
+        for i in nums:
+            if i >= second_largest and i != largest:
+                second_largest = i
+        if second_largest in [largest, float('-inf')]:
+            return -1
+        return second_largest
+        
     def getSecondLargest(self, arr):
-      dup_num = []
-      for i in range(len(arr)):
-        dup_num.append(arr[i])
-      object_call = self.largestFunc(dup_num)
-      largest_var = object_call[1]
-      
-      dup_num[object_call[0]] = float("-inf")
-      second_largest_var = dup_num[0]
-      
-      for i in range(len(dup_num)):
-        if i >= second_largest_var:
-          second_largest_var = dup_num[i]
-      print(second_largest_var, largest_var, dup_num)
-      if second_largest_var != largest_var:
-        return second_largest_var
-      return -1
-    
+        return self.func(arr)
+
 solution = Solution()
-nums = [10, 5, 10]
+nums = [10, 10, 10]
 call = solution.getSecondLargest(nums)
 print(call)
