f6f89d28221951322db9b4226f22740c895e28d0
YashPatkar <yash.patkar2004@gmail.com>
2025-11-09 14:14:46 +0530

    Complete [Brute]: rotate the array to the right by k steps

--- test.py
+++ test.py
@@ -1,11 +1,25 @@
-# Question: Remove Duplicates from Sorted Array [ in place ]
+# Question: rotate the array to the right by k steps
+
+from typing import List
 class Solution:
-    def removeDuplicates(self, nums):
-        current_val = nums[0]
-        count = 1
-        for i in nums[1:]:
-            if i != current_val:
-                nums[count] = i
-                count += 1
-                current_val = i
-        return count
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <= 2:
+            if len(nums) == 1:
+                return
+            for _ in range(0, k):
+                last = nums.pop()
+                nums.insert(0, last)
+        else:
+            k_before = []
+            k_after = []
+            n = len(nums) - k
+            print(n + 1)
+            for i in nums[:n]:
+                k_before.append(i)
+            for i in nums[n:]:
+                k_after.append(i)
+
+            nums[:] = k_after + k_before
