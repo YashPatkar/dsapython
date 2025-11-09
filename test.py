dbdf9db9bb0d0d9c4a4ff0df62e8ac7cc79a48d7
YashPatkar <yash.patkar2004@gmail.com>
2025-11-09 14:56:13 +0530

    Complete [better]: rotate the array to the right by k steps

--- test.py
+++ test.py
@@ -6,20 +6,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if len(nums) <= 2:
-            if len(nums) == 1:
-                return
-            for _ in range(0, k):
-                last = nums.pop()
-                nums.insert(0, last)
-        else:
-            k_before = []
-            k_after = []
-            n = len(nums) - k
-            print(n + 1)
-            for i in nums[:n]:
-                k_before.append(i)
-            for i in nums[n:]:
-                k_after.append(i)
+        k = k % len(nums)
+        k_before = []
+        k_after = []
+        n = len(nums) - k
+        
+        for i in nums[:n]:
+            k_before.append(i)
+        for i in nums[n:]:
+            k_after.append(i)
 
-            nums[:] = k_after + k_before
+        nums[:] = k_after + k_before
