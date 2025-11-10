30da62ad759f4954f89d2c3a5bed7a86564a286f
YashPatkar <yash.patkar2004@gmail.com>
2025-11-10 17:48:55 +0530

    Complete [Optimal]: Merge 2 Sorted Arrays Without Duplicates

--- test.py
+++ test.py
@@ -6,20 +6,20 @@ class Solution:
         result = []
         while i < len(nums1) and j < len(nums2):
             if nums1[i] <= nums2[j]:
-                if nums1[i] not in result:
+                if len(result) == 0 or nums1[i] != result[-1]:
                     result.append(nums1[i])
                 i+=1
             elif nums2[j] <= nums1[i]:
-                if nums2[j] not in result:
+                if len(result) == 0 or nums2[j] != result[-1]:
                     result.append(nums2[j])
                 j+=1
           
         for k in nums1[i:]:
-            if k not in result:
+            if k != result[-1]:
                 result.append(k)
         
         for k in nums2[j:]:
-            if k not in result:
+            if k != result[-1]:
                 result.append(k)
         
         return result
