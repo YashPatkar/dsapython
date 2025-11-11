975682a0ce1fc618e6a4df962dfad4a9a59137cd
YashPatkar <yash.patkar2004@gmail.com>
2025-11-11 21:36:10 +0530

    Complete [optimal]: 268. Missing Number

--- test.py
+++ test.py
@@ -1,29 +1,19 @@
 # Question: 268. Missing Number [Leetcode]
 
 class Solution(object):
-    def sortit(self, nums):
-        if len(nums) <= 1:
-            return nums
-        pivot = nums[0]
-        left = [i for i in nums[1:] if i <= pivot]
-        right = [i for i in nums[1:] if i > pivot]
-        return self.sortit(left) + [pivot] + self.sortit(right)
-  
+
     def missingNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) == 1:
-            if nums[0] != 0:
-                return 0
-            return nums[0] + 1
-        nums[:] = self.sortit(nums)
-        n = len(nums) - 1
-        while n >= 1:
-            if nums[n] - 1 != nums[n - 1]:
-                return nums[n] - 1
-            n-=1
-        if nums[0] != 0:
-            return 0
-        return nums[-1] + 1
+        num_sum = 0
+        for i in nums:
+            num_sum += i
+        
+        n = len(nums)
+        n_sum = 0
+        for i in range(n + 1):
+            n_sum += i
+        
+        return n_sum - num_sum
