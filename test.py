997589ff420eef62775db07fbe81c386fd3bcb92
YashPatkar <yash.patkar2004@gmail.com>
2026-02-04 22:11:41 +0530

    introduction to 2d Array

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-# Question: 14. Longest Common Prefix [Leetcode]
+nums = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
+for index1, row in enumerate(nums):
+    for index2, col in enumerate(row):
+        print(f"index1({row}), index2({col})")
