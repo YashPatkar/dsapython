a02550f64536d54b0bd19d288bb1b1ea9f2e56bc
YashPatkar <yash.patkar2004@gmail.com>
2026-02-11 21:52:35 +0530

    Wrong: 80. Leetcode[array]

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
-# 1480. Leetcode
 
+# 80. Leetcode
 class Solution(object):
-    def runningSum(self, nums):
+    def removeDuplicates(self, nums):
         """
         :type nums: List[int]
-        :rtype: List[int]
+        :rtype: int
         """
-        total = 0
-        for index, i in enumerate(nums):
-            total += i
-            nums[index] = total
+        freq = {}
         
-        return nums
+        for i in nums:
+            if i not in freq:
+                freq[i] = 1
+            else:
+                if freq[i] <= 1:
+                    freq[i] += 1
+        print(freq)
+        count = 0
+        for key, value in freq.items():
+            for i in range(value):
+                nums[count] = key
+                count += 1
+        print(nums)
+        return count
